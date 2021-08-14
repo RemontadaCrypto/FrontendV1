ac17022d2bcf52952b52a1f6e445cf6961d27ad6
JoBenEtuk <JoBenEtuk@users.noreply.github.com>
2021-08-14 14:50:23 +0100

    project init

---
+++ hooks/index.js
@@ -0,0 +1,6 @@
+import useMediaQuery from "./useMediaQuery.js";
+import useOnScreen from "./useOnScreen.js";
+import useWindowSize from "./useWindowSize.js";
+import useFetch from "./useFetch.js";
+
+export { useOnScreen, useMediaQuery, useWindowSize, useFetch };
