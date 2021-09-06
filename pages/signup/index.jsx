ef998b8da9bd753600d25ba2c30504cbb8d9fab2
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-06 19:41:19 +0100

    ✨ route to offer listing page when logged in

--- pages/signup/index.jsx
+++ pages/signup/index.jsx
@@ -1,19 +1,31 @@
+import React from "react";
+import { useSelector } from "react-redux";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 import { Loading } from "../../components";
 
 const DynamicComponent = dynamic(() => import("../../routes/signup"), {
-    loading: () => <Loading />,
+  loading: () => <Loading />,
 });
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Remontada | Signup</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <DynamicComponent />
-        </>
-    );
+  const router = useRouter();
+  const user = useSelector((state) => state.user);
+
+  React.useEffect(() => {
+    if (user.isAuth) {
+      router.push("/offer-listings");
+    }
+  }, [user.isAuth]);
+
+  return (
+    <>
+      <Head>
+        <title>Remontada | Signup</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <DynamicComponent />
+    </>
+  );
 }
