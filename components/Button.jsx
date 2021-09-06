9d5e8514ea98e19e2d3a0bd2d1ce58d0167b4a33
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-06 19:06:24 +0100

    ✨ more props added to button component

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import { motion } from "framer-motion"
+import React from "react";
+import { motion } from "framer-motion";
 
-const button = ({ onClick, type = "button", text, btnClass, icon }) => {
-    return (
-        <motion.button
-            whileHover={{ scale: 1.1, transition: { duration: 0.3, type: "tween" } }}
-            whileTap={{ scale: 0.9, transition: { duration: 0.3, type: "tween" } }}
-            type={type}
-            onClick={onClick}
-            className={btnClass ? btnClass : ""}>
-            {text}
-            {icon && icon}
-        </motion.button>
-    )
-}
+const buttonTransition = (clicked) => {
+  if (!clicked) {
+    return { scale: 0.9, transition: { duration: 0.3, type: "tween" } };
+  }
 
-export default button
+  return {};
+};
+
+const button = ({
+  onClick,
+  text,
+  btnClass,
+  icon,
+  style,
+  type = "button",
+  clicked = false,
+}) => {
+  return (
+    <motion.button
+      whileTap={buttonTransition(clicked)}
+      type={type}
+      onClick={onClick}
+      style={{ ...style }}
+      clicked={clicked}
+      className={btnClass ? btnClass : ""}
+    >
+      {text}
+      {icon && icon}
+    </motion.button>
+  );
+};
+
+export default button;
