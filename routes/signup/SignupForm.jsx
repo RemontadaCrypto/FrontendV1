833b3523db7dbe71dc79bbb25bf44cf6c2116e4d
JOSIAH-PC\etukj <etukjosiahbenjamin@gmail.com>
2021-09-06 10:23:51 +0100

    signup form update

--- routes/signup/SignupForm.jsx
+++ routes/signup/SignupForm.jsx
@@ -1,35 +1,62 @@
-import React, { useState } from 'react'
-import Link from "next/link"
-import { useRouter } from "next/router"
-import { Input, Button } from "../../components"
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Input, Button } from "../../components";
 
 const SignupForm = () => {
-    const router = useRouter()
-    const [buyer, setBuyer] = useState(true)
-    const [form, setForm] = useState({ email: "", password: "" })
-    const handleChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value })
-    }
+  const router = useRouter();
+  const [buyer, setBuyer] = useState(true);
+  const [form, setForm] = useState({ email: "", username: "", password: "" });
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        buyer ? router.push("/offer-listings") : router.push("/register")
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    buyer ? router.push("/offer-listings") : router.push("/register");
+  };
 
-    return (
-        <section className="signup">
-            <h3>Create new account</h3>
-            <p>Create an account and start trading cryptocurrencies with very little hassles</p>
-            <form onSubmit={handleSubmit} >
-                <Input name="email" onChange={handleChange} label="Email address" placeholder="Input email here" value={form.email} />
-                <Input type="password" name="password" onChange={handleChange} label="Set your password" value={form.password} />
-                <div className="form-button">
-                    <Button text="Register" type="submit" btnClass="btn btn--primary" />
-                </div>
-            </form>
-            <footer>Already have an account? <Link href="/signin"><a>Login here</a></Link> </footer>
-        </section>
-    )
-}
+  return (
+    <section className='signup'>
+      <h3>Create new account</h3>
+      <p>
+        Create an account and start trading cryptocurrencies with very little
+        hassles
+      </p>
+      <form onSubmit={handleSubmit}>
+        <Input
+          name='email'
+          onChange={handleChange}
+          label='Email address'
+          placeholder='Input email here'
+          value={form.email}
+        />
+        <Input
+          name='username'
+          onChange={handleChange}
+          label='Username'
+          placeholder='Input username here'
+          value={form.username}
+        />
+        <Input
+          type='password'
+          name='password'
+          onChange={handleChange}
+          label='Set your password'
+          value={form.password}
+        />
+        <div className='form-button'>
+          <Button text='Register' type='submit' btnClass='btn btn--primary' />
+        </div>
+      </form>
+      <footer>
+        Already have an account?{" "}
+        <Link href='/signin'>
+          <a>Login here</a>
+        </Link>{" "}
+      </footer>
+    </section>
+  );
+};
 
-export default SignupForm
+export default SignupForm;
