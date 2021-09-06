14593f09bd6ba792d95f82f76f19a10df1e91131
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-06 15:10:03 +0100

    ✨ toast feature added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/index.scss'
+import { Toaster } from 'react-hot-toast';
+import '../styles/index.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+    <Component {...pageProps} />
+    <Toaster position="top-right"
+      reverseOrder={false}
+      gutter={8}
+      toastOptions={{
+        duration: Infinity,
+        style: {
+          fontSize: 18,
+          marginTop: '20px',
+        }
+      }} />
+  </>
+  )
 }
 
 export default MyApp
