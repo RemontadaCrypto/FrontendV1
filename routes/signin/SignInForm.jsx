435830b0c96f8750f4bc45757b88a4560c030d21
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-06 19:31:27 +0100

    ✨ signin api completed

--- routes/signin/SignInForm.jsx
+++ routes/signin/SignInForm.jsx
@@ -1,39 +1,115 @@
-import React, { useState, useEffect } from 'react'
-import Link from "next/link"
-import { useRouter } from "next/router"
-import { Input, Button } from "../../components"
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useSelector, useDispatch } from "react-redux";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
-const SignupForm = () => {
-    const router = useRouter()
-    const [buyer, setBuyer] = useState(true)
-    const [form, setForm] = useState({ email: "", password: "" })
-    const handleChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value })
+import { loginUser } from "../../redux/actions/user.action";
+import axios from "../../axios/axiosInstance";
+import { useRouter } from "next/router";
+import { Input, Button } from "../../components";
+
+const SigninForm = () => {
+  const router = useRouter();
+  const [form, setForm] = useState({ email: "", password: "" });
+  const [loading, setLoading] = useState(false);
+
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (user.isAuth) {
+      router.push("/seller-escrow");
     }
+  }, [user.isAuth]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        buyer ? router.push("/offer-listings") : router.push("/register")
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    //dismiss all toasts
+    toast.dismiss();
+
+    setLoading(true);
+
+    try {
+      const { email, password } = form;
+
+      const body = {
+        email,
+        password,
+      };
+
+      const request = await axios({
+        method: "post",
+        url: "auth/login",
+        data: body,
+      });
+
+      setLoading(false);
+
+      if (request.status === 200) {
+        dispatch(loginUser(request.data.data, request.data.access_token));
+      }
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.message || "Registeration failed!", {
+        position: "top-right",
+        style: {
+          marginTop: 10,
+        },
+      });
     }
+  };
+  return (
+    <section className="signup signin">
+      <h3>Welcome Back</h3>
+      <p>Log back into your account and continue trading</p>
+      <form onSubmit={handleSubmit}>
+        <Input
+          name="email"
+          onChange={handleChange}
+          label="Email address"
+          placeholder="Input email here"
+          value={form.email}
+        />
+        <Input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          label="Set your password"
+          value={form.password}
+        />
+        <div className="form-button">
+          <Button
+            text="Login"
+            type="submit"
+            btnClass="btn btn--primary"
+            clicked={loading}
+            style={{ opacity: loading ? 0.5 : 1 }}
+            icon={
+              loading && (
+                <AiOutlineLoading3Quarters
+                  style={{
+                    marginLeft: 10,
+                  }}
+                />
+              )
+            }
+          />
+        </div>
+      </form>
+      <footer>
+        Do not have an account?{" "}
+        <Link href="/signup">
+          <a>Register here</a>
+        </Link>{" "}
+      </footer>
+    </section>
+  );
+};
 
-    useEffect(() => {
-        setBuyer(false)
-    }, [buyer])
-
-    return (
-        <section className="signup signin">
-            <h3>Welcome Back</h3>
-            <p>Log back into your account and continue trading</p>
-            <form onSubmit={handleSubmit} >
-                <Input name="email" onChange={handleChange} label="Email address" placeholder="Input email here" value={form.email} />
-                <Input type="password" name="password" onChange={handleChange} label="Set your password" value={form.password} />
-                <div className="form-button">
-                    <Button text="Login" type="submit" btnClass="btn btn--primary" />
-                </div>
-            </form>
-            <footer>Do not have an account? <Link href="/signup"><a>Register here</a></Link> </footer>
-        </section>
-    )
-}
-
-export default SignupForm
+export default SigninForm;
