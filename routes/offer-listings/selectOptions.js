bb6f98c2bc00cf63ee9c50eebfcbbac8c346e0b3
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-09 10:04:20 +0100

    ✨ selectoptions file created

---
+++ routes/offer-listings/selectOptions.js
@@ -0,0 +1,84 @@
+// FILTER ARRAYS
+export const coinArray = [
+   {
+      value: "BTC",
+      label: (
+         <div className="options">
+            <svg
+               width="24"
+               height="24"
+               viewBox="0 0 24 24"
+               fill="none"
+               xmlns="http://www.w3.org/2000/svg"
+            >
+               <rect width="24" height="24" rx="4" fill="#302F2F" />
+            </svg>
+            Bitcoin (BTC)
+         </div>
+      ),
+   },
+   {
+      value: "ETH",
+      label: (
+         <div className="options">
+            <svg
+               width="24"
+               height="24"
+               viewBox="0 0 24 24"
+               fill="none"
+               xmlns="http://www.w3.org/2000/svg"
+            >
+               <rect width="24" height="24" rx="4" fill="#302F2F" />
+            </svg>
+            Ethereum (ETH)
+         </div>
+      ),
+   },
+   {
+      value: "USDC",
+      label: (
+         <div className="options">
+            <svg
+               width="24"
+               height="24"
+               viewBox="0 0 24 24"
+               fill="none"
+               xmlns="http://www.w3.org/2000/svg"
+            >
+               <rect width="24" height="24" rx="4" fill="#302F2F" />
+            </svg>
+            USD Coin (USDC)
+         </div>
+      ),
+   },
+];
+
+export const filterArray = [
+   {
+      value: "Popularity",
+      label: "Popularity",
+   },
+   {
+      value: "Popularity",
+      label: "Popularity",
+   },
+   {
+      value: "Popularity",
+      label: "Popularity",
+   },
+];
+
+export const tradeArray = [
+   {
+      value: "Any",
+      label: "Any",
+   },
+   {
+      value: "Any",
+      label: "Any",
+   },
+   {
+      value: "Any",
+      label: "Any",
+   },
+];
