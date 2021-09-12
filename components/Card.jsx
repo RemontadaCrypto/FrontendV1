d5ed033910b23ae981c246feeed7560433b55d47
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-12 13:45:47 +0100

    ♻️ card component refactored

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,18 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Card = ({ tab, title, info, time, active }) => {
-    return (
-        <section className={`card ${active ? "active" : ""}`}>
-            <div className="tab">
-                {tab}
-            </div>
-            <h2>{title}</h2>
-            <p>{info}</p>
-            {time && <p>
-                <strong>Time left:</strong> {time}
-            </p>}
-        </section>
-    )
-}
+const Card = ({ tab, title, info, className = "" }) => {
+  return (
+    <section className={`card ${className}`}>
+      <div className="tab">{tab}</div>
+      <h2>{title}</h2>
+      <p>{info}</p>
+    </section>
+  );
+};
 
-export default Card
+export default Card;
