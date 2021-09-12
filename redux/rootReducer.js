70d48df456c01c27945f81492b3f4797a780bd1e
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-12 13:45:30 +0100

    ✨ trade info redux data added

--- redux/rootReducer.js
+++ redux/rootReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import offerReducer from './reducers/selected-offer.reducer';
 import userReducer from './reducers/user.reducer';
-import bufferOfferReducer from './reducers/buyer-offer.reducer';
+import buyerOfferReducer from './reducers/buyer-offer.reducer';
+import tradeInfoReducer from './reducers/trade-info.reducer';
 
 export const rootReducer = combineReducers({
   user: userReducer,
   offer: offerReducer,
-  buyerOffer: bufferOfferReducer
+  buyerOffer: buyerOfferReducer,
+  tradeInfo: tradeInfoReducer,
 });
 
