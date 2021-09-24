4c140c0568e7ba489fa75b3d7ffcd1b591ab1f4c
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-24 12:08:33 +0100

    ✨ validation added to login form

--- components/SignInForm.jsx
+++ components/SignInForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import Link from "next/link";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import toast from "react-hot-toast";
 import { useSelector, useDispatch } from "react-redux";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
@@ -8,14 +9,31 @@ import { loginUser } from "../redux/actions/user.action";
 import axios from "../axios/axiosInstance";
 import { useRouter } from "next/router";
 import { Input, Button } from ".";
+import { ErrorHandler } from "../utils/errorHandler";
+import BackdropSpinner from "./BackdropSpinner";
+
+const SigninSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Email is Required"),
+  password: Yup.string().required("Please Enter your password"),
+});
 
 const SigninForm = () => {
   const router = useRouter();
-  const [form, setForm] = useState({ email: "", password: "" });
   const [loading, setLoading] = useState(false);
 
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
+
+  const formik = useFormik({
+    initialValues: {
+      password: "",
+      email: "",
+    },
+    validationSchema: SigninSchema,
+    onSubmit: (values, { resetForm }) => {
+      _handleSubmit(values, resetForm);
+    },
+  });
 
   React.useEffect(() => {
     if (user.isAuth) {
@@ -23,84 +41,99 @@ const SigninForm = () => {
     }
   }, [user.isAuth]);
 
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const _handleSubmit = async (values, resetForm) => {
     //dismiss all toasts
     toast.dismiss();
 
     setLoading(true);
 
     try {
-      const { email, password } = form;
-
-      const body = {
-        email,
-        password,
-      };
-
       const request = await axios({
         method: "post",
         url: "auth/login",
-        data: body,
+        data: values,
       });
 
       setLoading(false);
 
       if (request.status === 200) {
         dispatch(loginUser(request.data.data, request.data.access_token));
+        resetForm();
       }
-    } catch (error) {
+    } catch (e) {
       setLoading(false);
-      toast.error(error.message || "Registeration failed!", {
+      const error = ErrorHandler(e);
+      toast.error(error || "Unable to login", {
         duration: 6000,
         position: "top-center",
       });
     }
   };
+
   return (
-    <section className="signup signin">
-      <h3>Welcome Back</h3>
-      <p>Log back into your account and continue trading</p>
-      <form onSubmit={handleSubmit}>
-        <Input
-          name="email"
-          onChange={handleChange}
-          label="Email address"
-          placeholder="Input email here"
-          value={form.email}
-        />
-        <Input
-          type="password"
-          name="password"
-          onChange={handleChange}
-          label="Set your password"
-          value={form.password}
-        />
-        <div className="form-button">
-          <Button
-            text="Login"
-            type="submit"
-            btnClass="btn btn--primary"
-            clicked={loading}
-            style={{ opacity: loading ? 0.5 : 1 }}
-            icon={
-              loading && (
-                <AiOutlineLoading3Quarters
-                  style={{
-                    marginLeft: 10,
-                  }}
-                />
-              )
-            }
+    <>
+      {loading && <BackdropSpinner />}
+      <section className="signup signin">
+        <h3>Welcome Back</h3>
+        <p>Log back into your account and continue trading</p>
+        <form onSubmit={formik.handleSubmit}>
+          <Input
+            name="email"
+            label="Email address"
+            placeholder="Input email here"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
           />
-        </div>
-      </form>
-    </section>
+          {formik.touched.email && formik.errors.email ? (
+            <span
+              className="font-weight-bold text-danger d-block mb-3"
+              style={{
+                marginTop: "-26px",
+              }}
+            >
+              {formik.errors.email}
+            </span>
+          ) : null}
+          <Input
+            type="password"
+            name="password"
+            label="Set your password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <span
+              className="font-weight-bold text-danger d-block mb-3"
+              style={{
+                marginTop: "-26px",
+              }}
+            >
+              {formik.errors.password}
+            </span>
+          ) : null}
+          <div className="form-button">
+            <Button
+              text="Login"
+              type="submit"
+              btnClass="btn btn--primary"
+              clicked={loading}
+              style={{ opacity: loading ? 0.5 : 1 }}
+              icon={
+                loading && (
+                  <AiOutlineLoading3Quarters
+                    style={{
+                      marginLeft: 10,
+                    }}
+                  />
+                )
+              }
+            />
+          </div>
+        </form>
+      </section>
+    </>
   );
 };
 
