e4feba982641057ed6cca7aff0f768ab0fd437d8
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-24 11:58:06 +0100

    ✨ backdrop loader added to signup page

--- components/SignupForm.jsx
+++ components/SignupForm.jsx
@@ -7,6 +7,7 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { Input, Button } from ".";
 import axios from "../axios/axiosInstance";
 import { ErrorHandler } from "../utils/errorHandler";
+import BackdropSpinner from "../components/BackdropSpinner";
 
 const SignupSchema = Yup.object().shape({
   name: Yup.string()
@@ -68,87 +69,90 @@ const SignupForm = () => {
   };
 
   return (
-    <section className="signup signup-height">
-      <h3>Create new account</h3>
-      <p>
-        Create an account and start trading cryptocurrencies with very little
-        hassles
-      </p>
-      <form ref={formRef} onSubmit={formik.handleSubmit}>
-        <Input
-          name="email"
-          label="Email address"
-          placeholder="Input email here"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}
-        />
-        {formik.touched.email && formik.errors.email ? (
-          <span
-            className="font-weight-bold text-danger d-block mb-3"
-            style={{
-              marginTop: "-26px",
-            }}
-          >
-            {formik.errors.email}
-          </span>
-        ) : null}
-        <Input
-          name="name"
-          label="Username"
-          placeholder="Input username here"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.name}
-        />
-        {formik.touched.name && formik.errors.name ? (
-          <span
-            className="font-weight-bold text-danger d-block mb-3"
-            style={{
-              marginTop: "-26px",
-            }}
-          >
-            {formik.errors.name}
-          </span>
-        ) : null}
-        <Input
-          type="password"
-          name="password"
-          label="Set your password"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.password}
-        />
-        {formik.touched.password && formik.errors.password ? (
-          <span
-            className="font-weight-bold text-danger d-block mb-3"
-            style={{
-              marginTop: "-26px",
-            }}
-          >
-            {formik.errors.password}
-          </span>
-        ) : null}
-        <div className="form-button">
-          <Button
-            text="Register"
-            type="submit"
-            btnClass="btn btn--primary"
-            clicked={loading}
-            style={{ opacity: loading ? 0.5 : 1 }}
-            icon={
-              loading && (
-                <AiOutlineLoading3Quarters
-                  style={{
-                    marginLeft: 10,
-                  }}
-                />
-              )
-            }
+    <>
+      {loading && <BackdropSpinner />}
+      <section className="signup signup-height">
+        <h3>Create new account</h3>
+        <p>
+          Create an account and start trading cryptocurrencies with very little
+          hassles
+        </p>
+        <form ref={formRef} onSubmit={formik.handleSubmit}>
+          <Input
+            name="email"
+            label="Email address"
+            placeholder="Input email here"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
           />
-        </div>
-      </form>
-    </section>
+          {formik.touched.email && formik.errors.email ? (
+            <span
+              className="font-weight-bold text-danger d-block mb-3"
+              style={{
+                marginTop: "-26px",
+              }}
+            >
+              {formik.errors.email}
+            </span>
+          ) : null}
+          <Input
+            name="name"
+            label="Username"
+            placeholder="Input username here"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
+          />
+          {formik.touched.name && formik.errors.name ? (
+            <span
+              className="font-weight-bold text-danger d-block mb-3"
+              style={{
+                marginTop: "-26px",
+              }}
+            >
+              {formik.errors.name}
+            </span>
+          ) : null}
+          <Input
+            type="password"
+            name="password"
+            label="Set your password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <span
+              className="font-weight-bold text-danger d-block mb-3"
+              style={{
+                marginTop: "-26px",
+              }}
+            >
+              {formik.errors.password}
+            </span>
+          ) : null}
+          <div className="form-button">
+            <Button
+              text="Register"
+              type="submit"
+              btnClass="btn btn--primary"
+              clicked={loading}
+              style={{ opacity: loading ? 0.5 : 1 }}
+              icon={
+                loading && (
+                  <AiOutlineLoading3Quarters
+                    style={{
+                      marginLeft: 10,
+                    }}
+                  />
+                )
+              }
+            />
+          </div>
+        </form>
+      </section>
+    </>
   );
 };
 
