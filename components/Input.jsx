3f08ea2590a920461a2074f49cf29b310951ac4f
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-24 11:50:38 +0100

    🔥 required field removed

--- components/Input.jsx
+++ components/Input.jsx
@@ -2,44 +2,57 @@ import React, { useState } from "react";
 import { RiEyeLine, RiEyeOffLine } from "react-icons/ri";
 
 const Input = ({
-    label,
-    name,
-    value,
-    onChange,
-    disabled,
-    placeholder,
-    type = "text",
+  label,
+  name,
+  value,
+  onChange,
+  onBlur,
+  disabled,
+  placeholder,
+  type = "text",
 }) => {
-    const [password, setPassword] = useState(true)
-    return (
-        <div className="input-field">
-            {label && <label>{label}</label>}
-            {type !== "password" ?
-                <div className="input">
-                    <input
-                        type={type}
-                        name={name && name}
-                        placeholder={placeholder}
-                        value={value}
-                        disabled={disabled && disabled}
-                        required={true}
-                        onChange={onChange}
-                    />
-                </div> :
-                <div className="input flex-ac-jb">
-                    <input
-                        type={password ? "password" : "text"}
-                        name={name && name}
-                        value={value}
-                        required={true}
-                        disabled={disabled && disabled}
-                        onChange={onChange}
-                    />
-                    {password ? <RiEyeLine onClick={() => { setPassword(!password) }} /> :
-                        <RiEyeOffLine onClick={() => { setPassword(!password) }} />}
-                </div>
-            }
+  const [password, setPassword] = useState(true);
+  return (
+    <div className="input-field">
+      {label && <label>{label}</label>}
+      {type !== "password" ? (
+        <div className="input">
+          <input
+            type={type}
+            name={name && name}
+            placeholder={placeholder}
+            value={value}
+            disabled={disabled && disabled}
+            onChange={onChange}
+            onBlur={onBlur}
+          />
         </div>
-    );
+      ) : (
+        <div className="input flex-ac-jb">
+          <input
+            type={password ? "password" : "text"}
+            name={name && name}
+            value={value}
+            disabled={disabled && disabled}
+            onChange={onChange}
+            onBlur={onBlur}
+          />
+          {password ? (
+            <RiEyeLine
+              onClick={() => {
+                setPassword(!password);
+              }}
+            />
+          ) : (
+            <RiEyeOffLine
+              onClick={() => {
+                setPassword(!password);
+              }}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
-export default Input
+export default Input;
