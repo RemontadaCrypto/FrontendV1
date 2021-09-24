13b28a709d4920d263fc15786c0ca034c0415e69
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-24 11:57:46 +0100

    ✏️ typo fixed

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,7 +1,7 @@
 var fd = require("lodash/flattenDeep");
 
 export const ErrorHandler = (error) => {
-   if (error.response?.data && Object.keys(e.response.data).length > 0) {
+   if (error.response?.data && Object.keys(error.response.data).length > 0) {
       const values = Object.values(error.response?.data);
       return fd(Object.values(values)).join("\n");
    } else if (error.message) {
