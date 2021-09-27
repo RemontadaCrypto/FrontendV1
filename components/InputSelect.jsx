ea69d42159873e3cd65a23fb667723a8120edc74
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-27 10:43:17 +0100

    🔥 dummy data removed

--- components/InputSelect.jsx
+++ components/InputSelect.jsx
@@ -1,41 +1,38 @@
-import React from 'react'
-import Select from 'react-select'
+import React from "react";
+import Select from "react-select";
 
-const dummy = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-]
+const dummy = [];
 
 const customStyles = {
-    menu: (provided, state) => ({
-        ...provided,
-        color: "#294766",
-        background: state.isSelected ? "#DBE9FF" : "#fff",
-    }),
+  menu: (provided, state) => ({
+    ...provided,
+    color: "#294766",
+    background: state.isSelected ? "#DBE9FF" : "#fff",
+  }),
 
-    singleValue: (provided) => ({
-        ...provided,
-        color: "#294766",
-        fontWeight: 500,
-        fontSize: "1rem",
-    })
-}
+  singleValue: (provided) => ({
+    ...provided,
+    color: "#294766",
+    fontWeight: 500,
+    fontSize: "1rem",
+  }),
+};
 
 const InputSelect = ({ placeholder, options, onChange, value }) => (
-    <Select
-        styles={customStyles}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder ? placeholder : "select..."}
-        options={options ? options : dummy}
-        theme={theme => ({
-            ...theme,
-            colors: {
-                ...theme.colors,
-                primary25: '#dbe9ff',
-                primary: '#294766',
-            },
-        })} />
-)
-export default InputSelect
+  <Select
+    styles={customStyles}
+    value={value}
+    onChange={onChange}
+    placeholder={placeholder ? placeholder : "select..."}
+    options={options ? options : dummy}
+    theme={(theme) => ({
+      ...theme,
+      colors: {
+        ...theme.colors,
+        primary25: "#dbe9ff",
+        primary: "#294766",
+      },
+    })}
+  />
+);
+export default InputSelect;
