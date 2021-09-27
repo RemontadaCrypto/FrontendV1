1d12f8f5790c9a8cd3d68a564540dca99b7151be
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-27 10:19:42 +0100

    ✨ coins redux implementation completed

---
+++ redux/actions/coins.action.js
@@ -0,0 +1,29 @@
+import { CoinsType } from '../types';
+import axios from "../../axios/axiosInstance";
+import { ErrorHandler } from '../../utils/errorHandler';
+
+export const fetchCoins = (token, callback = () => { }) => {
+   return async (dispatch) => {
+      dispatch({
+         type: CoinsType.FETCH_COINS
+      });
+
+      try {
+         const response = await axios({
+            method: 'get',
+            url: 'coins'
+         });
+
+         dispatch({
+            type: CoinsType.SET_COINS,
+            coins: response.data
+         })
+      } catch (e) {
+         const error = ErrorHandler(e);
+         dispatch({
+            type: CoinsType.FETCH_COINS_FAILED,
+            error: error || 'unable to fetch available coins'
+         });
+      }
+   };
+};
